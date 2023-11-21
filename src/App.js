c88dab9b27dae43642fad6e45b58a83d8ed4fa62
John DeLucia <john.delucia@resupplyme.com>
2023-11-20 23:12:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacccct
+          Learn Reaccct
         </a>
       </header>
     </div>
