1fa7d0252275aff22173b711067386d0f0318c07
John DeLucia <john.delucia@resupplyme.com>
2023-11-27 15:31:35 -0500

    change test

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my website.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Igor + John
-        </a>
+
       </header>
     </div>
   );
