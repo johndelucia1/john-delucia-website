72026513a5285b0b362bfdbeffb13c65bfd16644
John DeLucia <john.delucia@resupplyme.com>
2023-11-28 16:36:46 -0500

    added basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Contact from './components/Contact';
 
 const App = () => {
     return (
-        <Router>
+        <Router basename="/">
             <div>
                 <NavBar />
                 <Routes>
