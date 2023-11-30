36beb7f522d0066c8e18d6043535f61e4837f954
John DeLucia <john.delucia@resupplyme.com>
2023-11-30 11:45:51 -0500

    personalized website

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,10 +8,10 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
+    // const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
+    // const zip = this.props.data.address.zip;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
@@ -38,9 +38,7 @@ class About extends Component {
                     <span>{name}</span>
                     <br />
                     <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
+                      {city}, {state}
                     </span>
                     <br />
                     <span>{phone}</span>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const project = this.props.data.project;
-    const github = this.props.data.github;
+    // const project = this.props.data.project;
+    // const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
 
@@ -65,16 +65,16 @@ class Header extends Component {
               <h3>{description}.</h3>
             </Fade>
             <hr />
-            <Fade bottom duration={2000}>
-              <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
-                </a>
-              </ul>
-            </Fade>
+            {/*<Fade bottom duration={2000}>*/}
+            {/*  <ul className="social">*/}
+            {/*    <a href={project} className="button btn project-btn">*/}
+            {/*      <i className="fa fa-book"></i>Project*/}
+            {/*    </a>*/}
+            {/*    <a href={github} className="button btn github-btn">*/}
+            {/*      <i className="fa fa-github"></i>Github*/}
+            {/*    </a>*/}
+            {/*  </ul>*/}
+            {/*</Fade>*/}
           </div>
         </div>
 
